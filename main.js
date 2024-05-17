a2e3fa5548c337cebf6c084f8b0a66e939b1476b
prestonm44 <preston.mitchell4444@gmail.com>
2024-05-16 21:29:17 -0600

    Nearly done

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 import getRandomWord from './src/randomWord.js';
 import setSharkImage from './src/sharkImage.js';
+import setupWord from './src/word.js';
+import { isLetterInWord, revealLetterInWord } from './src/word.js';
+import setupGuesses from './src/guess.js';
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
@@ -12,14 +15,46 @@ document.querySelector('#app').innerHTML = `
   <section id="letter-buttons"></section>
 `;
 
-const initSharkwords = () => {
-  let numWrong = 0;
-  const word = getRandomWord();
 
+const initSharkwords = () => {
+  
+  const sharkImgE1 = document.querySelector('#shark-img')
+
+  let numWrong = 0;
+  
+  const word = getRandomWord();
+  
+  console.log(document.querySelector('#shark-img'))
+  setSharkImage(sharkImgE1, numWrong);
+  setupWord(document.querySelector('#word-container'), word);
   // for debugging:
+  
   console.log(`[INFO] Correct word is: ${word}`);
+
+  const handleGuess = (guessEvent, letter) => {
+    const button = guessEvent.target;
+    button.setAttribute('disabled', true);
+    const isWordComplete = Array.from(document.querySelectorAll('.letter-box')).every(
+      (el) => el.innerText !== '',
+    );
+
+  if (isLetterInWord(letter)) {
+    revealLetterInWord(letter);
+  } else {
+    numWrong += 1;
+    setSharkImage(sharkImgE1, numWrong)
+  }
 };
+
+  setupGuesses(document.querySelector('#letter-buttons'), handleGuess);
+  
+
+  return initSharkwords;
+};
+
+document.querySelectorAll('button').forEach((btn) => {
+  btn.setAttribute('disabled', true);
+});
 
 initSharkwords(setSharkImage);
 
-setSharkImage(document.querySelector('#shark-img'), numWrong);
--- src/word.js
+++ src/word.js
@@ -1,7 +1,25 @@
 let word;
 
-function setupWord(element, initWord) {}
+function setupWord(element, initWord) {
+    word = initWord;
+    word.split('').forEach(() => {
+        element.insertAdjacentHTML('beforeend', `<div class="letter-box"></div>`);
+    })
+}
 
-function isLetterInWord(letter) {}
+function isLetterInWord(letter) {
+    return word.includes(letter);
+}
 
-function revealLetterInWord(letter) {}
+function revealLetterInWord(letter) {
+    const letterBoxes = document.querySelectorAll('.letter-box');
+    word.split('').forEach((wordLetter, idx) => {
+        if (wordLetter === letter) {
+            letterBoxes[idx].innerHTML = letter;
+        }
+    })
+}
+
+
+export default setupWord; 
+export { isLetterInWord, revealLetterInWord };
